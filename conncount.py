ed94c4547296fde53c9065f16c85e78db578015c
ruilung <ruilung@gmail.com>
2017-01-02 13:16:41 +0800

    add local port connection

--- conncount.py
+++ conncount.py
@@ -13,11 +13,13 @@ print ""
 
 dictConn={}
 dictStatus={}
+dictLocalPort={}
 
 for i in conn:
     if i.raddr:
         remoteAddress=i.raddr[0]
         connStatus=i.status
+        localPort=i.laddr[1]
         if dictConn.has_key(remoteAddress):
             dictConn[remoteAddress]=dictConn[remoteAddress]+1
             # count  +1 if  dict already had this ip
@@ -30,10 +32,17 @@ for i in conn:
         else:
             dictStatus[connStatus]=1
             # add status as new key, also count it from 1
+        if dictLocalPort.has_key(localPort):
+            dictLocalPort[localPort]=dictStatus[localPort]+1
+            # count  +1 if  dict already had this status
+        else:
+            dictLocalPort[localPort]=1
+            # add status as new key, also count it from 1
 
 
 sorted_conn = sorted(dictConn.items(), key=operator.itemgetter(1), reverse=True)
 sorted_status = sorted(dictStatus.items(), key=operator.itemgetter(1), reverse=True)
+sorted_localPort= sorted(dictLocalPort.items(), key=operator.itemgetter(1), reverse=True)
 
 print "top 10, remote address"
 icount=1
@@ -54,3 +63,15 @@ for i in sorted_status:
     if (icount>10):
         break
         # list top 10 status, sorted by status
+
+print ""
+
+print "top 10,local port"
+icount=1
+for i in sorted_localPort:
+    print "%2d %-15s %s" %(icount,i[0],i[1])
+    icount+=1
+    if (icount>10):
+        break
+        # list top 10 status, sorted by status
+
