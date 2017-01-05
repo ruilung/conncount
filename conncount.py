2ef8db7a3f3b636c7ccb4e8f33c74a9f56675202
ruilung@gmail.com <3gxijall>
2017-01-05 10:30:31 +0800

    add subfunction printTop to print infomation

--- conncount.py
+++ conncount.py
@@ -2,6 +2,25 @@ import psutil
 import operator
 import datetime
 
+def printTop(dictData,recordToPrint,totalConnection):
+    #dictData get connection info from dictConn,dictStatus and so on
+    #recordToprint decide how many record will be print
+    #totalConnection get remoteAddressSum to calc perentage
+    listSorted=sorted(dictData.items(), key=operator.itemgetter(1), reverse=True)
+    icount = 1
+    rcount = 0.0
+    for i in listSorted:
+        print "%2d %-15s %s" % (icount, i[0], i[1])
+        icount += 1
+        rcount += i[1]
+        if (icount > recordToPrint):
+            break
+            # list top 10 remote address, sorted by connections.
+
+    print "top %d %3.2f%%, other %3.2f%%" % (recordToPrint, rcount / totalConnection * 100, (totalConnection - rcount) / totalConnection * 100)
+    print ""
+
+#  program start at here
 print ""
 print "conncount ver 0.1"
 print "execute time:",datetime.datetime.now()
@@ -12,7 +31,7 @@ print "total connections:", len(conn)
 print ""
 
 
-dictConn={}
+dictConnRaddr={}
 dictStatus={}
 dictLocalPort={}
 
@@ -23,12 +42,11 @@ for i in conn:
         connStatus=i.status
         localPort=i.laddr[1]
         remoteAddressSum += 1
-        if dictConn.has_key(remoteAddress):
-            dictConn[remoteAddress]=dictConn[remoteAddress]+1
+        if dictConnRaddr.has_key(remoteAddress):
+            dictConnRaddr[remoteAddress]=dictConnRaddr[remoteAddress]+1
             # count  +1 if  dict already had this ip
-
         else:
-            dictConn[remoteAddress]=1
+            dictConnRaddr[remoteAddress]=1
             # add ip as new key, also count it from 1
         if dictStatus.has_key(connStatus):
             dictStatus[connStatus]=dictStatus[connStatus]+1
@@ -38,51 +56,17 @@ for i in conn:
             # add status as new key, also count it from 1
         if dictLocalPort.has_key(localPort):
             dictLocalPort[localPort]=dictLocalPort[localPort]+1
-            # count  +1 if  dict already had this status
+            # count  +1 if  dict already had this localPort
         else:
             dictLocalPort[localPort]=1
-            # add status as new key, also count it from 1
+            # add localPort as new key, also count it from 1
 
-
-sorted_conn = sorted(dictConn.items(), key=operator.itemgetter(1), reverse=True)
-sorted_status = sorted(dictStatus.items(), key=operator.itemgetter(1), reverse=True)
-sorted_localPort= sorted(dictLocalPort.items(), key=operator.itemgetter(1), reverse=True)
 
 print "top 10, remote address"
-icount=1
-rcount=0.0
-for i in sorted_conn:
-    print "%2d %-15s %s" %(icount,i[0],i[1])
-    icount+=1
-    rcount+=i[1]
-    if (icount>10):
-        break
-        # list top 10 remote address, sorted by connections.
-
-print "top 10 %3.2f%%, other %3.2f%%" %(rcount/remoteAddressSum*100, (remoteAddressSum-rcount)/remoteAddressSum*100)
-print ""
+printTop(dictConnRaddr,10,remoteAddressSum)
 
 print "top 10,connection status"
-icount=1
-rcount=0.0
-for i in sorted_status:
-    print "%2d %-15s %s" %(icount,i[0],i[1])
-    icount+=1
-    rcount += i[1]
-    if (icount>10):
-        break
-        # list top 10 status, sorted by status
-print "top 10 %3.2f%%, other %3.2f%%" %(rcount/remoteAddressSum*100, (remoteAddressSum-rcount)/remoteAddressSum*100)
-print ""
+printTop(dictStatus,10,remoteAddressSum)
 
 print "top 10,local port"
-icount=1
-rcount=0.0
-for i in sorted_localPort:
-    print "%2d %-15s %s" %(icount,i[0],i[1])
-    icount+=1
-    rcount += i[1]
-    if (icount>10):
-        break
-        # list top 10 status, sorted by status
-print "top 10 %3.2f%%, other %3.2f%%" %(rcount/remoteAddressSum*100, (remoteAddressSum-rcount)/remoteAddressSum*100)
+printTop(dictLocalPort,10,remoteAddressSum)
